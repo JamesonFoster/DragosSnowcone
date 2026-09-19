e7d9522ba17aa9553d74827925c73fc16f4a459e
JamesonFoster <jamesonbfoster@gmail.com>
2026-09-19 13:19:50 -0500

    Prefab Placeholder Push

--- DragosSnowcone/Assets/Scripts/CustomerScripts/CustomerMovement.cs
+++ DragosSnowcone/Assets/Scripts/CustomerScripts/CustomerMovement.cs
@@ -12,7 +12,6 @@ public class CustomerMovement : MonoBehaviour
     public List<GameObject> waitLine; // line where they wait for the snowcone
     private GameObject currentTarg;
     public GameObject targetMain2;
-    public float walkSpeed;
     public float bob;
     public GameObject takeOrderButton;
 
@@ -23,6 +22,7 @@ public class CustomerMovement : MonoBehaviour
     private float itmer1;
     private SpriteRenderer sprrend;
     private CustWaitSpot CWSStar1;
+    private CustWaitSpot CWSStar2;
     private CustWaitSpot CWS;
     private CustWaitSpot currCWS;
     private int currentWaitPos;
@@ -36,6 +36,7 @@ public class CustomerMovement : MonoBehaviour
     void Start()
     {
         CWSStar1 = targetMain1.GetComponent<CustWaitSpot>();
+        CWSStar2 = targetMain2.GetComponent<CustWaitSpot>();
         startingPos = new Vector2(transform.position.x, transform.position.y);
         sprrend.sprite = customer.walkSpr1;
         for (int i = 0; i < customer.orders.Count; i++)
@@ -51,6 +52,7 @@ public class CustomerMovement : MonoBehaviour
     {
         if (mode == 0)
         {
+            sprrend.sortingOrder = 5;
             if (CWSStar1.isOn == false)
             {
                 currentTarg = targetMain1;
@@ -72,7 +74,7 @@ public class CustomerMovement : MonoBehaviour
             }
 
             HandleWalkSpr();
-            transform.position = Vector2.MoveTowards(transform.position, currentTarg.transform.position, walkSpeed * Time.deltaTime);
+            transform.position = Vector2.MoveTowards(transform.position, currentTarg.transform.position, customer.walkSpeed * Time.deltaTime);
             Vector2 position2D = new Vector2(transform.position.x, transform.position.y);
             Vector2 posFirst2D = new Vector2(currentTarg.transform.position.x, currentTarg.transform.position.y);
             Vector2 starPos2D = new Vector2(targetMain1.transform.position.x, targetMain1.transform.position.y);
@@ -89,6 +91,9 @@ public class CustomerMovement : MonoBehaviour
         }
         if (mode == 1)
         {
+            sprrend.sortingOrder = 4;
+            if (customer.joyful == true)
+                HandleWalkSpr();
             if (CWSStar1.isOn == false)
             {
                 currentTarg = targetMain1;
@@ -115,26 +120,114 @@ public class CustomerMovement : MonoBehaviour
         }
         if (mode == 2)
         {
+            sprrend.sortingOrder = 4;
+            if (customer.joyful == true)
+                HandleWalkSpr();
             takeOrderButton.SetActive(true);
             //whatever is handling the customers ordering will tell this to go to mode 2
         }
         if (mode == 3)
         {
+            sprrend.sortingOrder = 3;
             takeOrderButton.SetActive(false);
             sprrend.flipX = true;
             HandleWalkSpr();
             //transform.position = Vector2.MoveTowards(transform.position, secndTarget.transform.position, walkSpeed * Time.deltaTime);
-            transform.position = Vector2.MoveTowards(transform.position, startingPos, walkSpeed * Time.deltaTime);
+            transform.position = Vector2.MoveTowards(transform.position, startingPos, customer.walkSpeed * Time.deltaTime);
             Vector2 position2D = new Vector2(transform.position.x, transform.position.y);
             if (position2D == startingPos)
             {
                 sprrend.flipX = false;
+                transform.position = new Vector2 (transform.position.x, targetMain2.transform.position.y);
                 mode = 4;
             }
         }
         if (mode == 4)
         {
-            // waits for their order, or changes to mode 2 when a new open spot in the waiting line opens up
+            sprrend.sortingOrder = 2;
+            if (CWSStar2.isOn == false)
+            {
+                currentTarg = targetMain2;
+            }
+            else
+            {
+                for (int i = 0; i < waitLine.Count; i++)
+                {
+                    GameObject currOb = waitLine[i];
+                    CWS = currOb.GetComponent<CustWaitSpot>();
+                    if (CWS.isOn == false)
+                    {
+                        currentTarg = currOb;
+                        currCWS = CWS;
+                        currentWaitPos = i;
+                        break;
+                    }
+                }
+            }
+
+            HandleWalkSpr();
+            transform.position = Vector2.MoveTowards(transform.position, currentTarg.transform.position, customer.walkSpeed * Time.deltaTime);
+            Vector2 position2D = new Vector2(transform.position.x, transform.position.y);
+            Vector2 posFirst2D = new Vector2(currentTarg.transform.position.x, currentTarg.transform.position.y);
+            Vector2 starPos2D = new Vector2(targetMain2.transform.position.x, targetMain2.transform.position.y);
+            if ((position2D == posFirst2D) && currentTarg != targetMain2)
+            {
+                currCWS.setIsON(true);
+                mode = 5;
+            }
+            if (position2D == starPos2D)
+            {
+                CWSStar2.setIsON(true);
+                mode = 6;
+            }
+        }
+        if (mode == 5)
+        {
+            sprrend.sortingOrder = 1;
+            if (customer.joyful == true)
+                HandleWalkSpr();
+            if (CWSStar2.isOn == false)
+            {
+                currentTarg = targetMain2;
+                currCWS.setIsON(false);
+                mode = 4;
+            }
+            else
+            {
+                for (int i = 0; i < waitLine.Count; i++)
+                {
+                    GameObject currOb = waitLine[i];
+                    CWS = currOb.GetComponent<CustWaitSpot>();
+                    if (CWS.isOn == false && currentWaitPos > i)
+                    {
+                        currentTarg = currOb;
+                        currCWS.setIsON(false);
+                        currCWS = CWS;
+                        currentWaitPos = i;
+                        mode = 4;
+                        break;
+                    }
+                }
+            }
+        }
+        if (mode == 6)
+        {
+            sprrend.sortingOrder = 1;
+            if (customer.joyful == true)
+                HandleWalkSpr();
+        }
+        if (mode == 7)
+        {
+            sprrend.sortingOrder = 0;
+            sprrend.flipX = true;
+            HandleWalkSpr();
+            Vector2 goToHere = new Vector2 (startingPos.x, targetMain2.transform.position.y);
+            transform.position = Vector2.MoveTowards(transform.position, goToHere, customer.walkSpeed * Time.deltaTime);
+            Vector2 position2D = new Vector2(transform.position.x, transform.position.y);
+            if (position2D == goToHere)
+            {
+                Destroy(gameObject);
+            }
         }
     }
 
