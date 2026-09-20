d2cf17ecf1113ba9a7cc2df6d82bdbbaf1fc41f7
CooperCantrell <coopercantrell56@gmail.com>
2026-09-19 23:11:53 -0500

    lose fish wait time

--- DragosSnowcone/Assets/Scripts/PointerCollector.cs
+++ DragosSnowcone/Assets/Scripts/PointerCollector.cs
@@ -47,7 +47,8 @@ public class PointerCollector : MonoBehaviour
         }
     }
 
-    void pointerMovement() {
+    void pointerMovement()
+    {
         // Move the pointer towards the target position
         pointerTransform.position = Vector3.MoveTowards(pointerTransform.position, targetPosition, moveSpeed * Time.deltaTime);
 
@@ -64,7 +65,8 @@ public class PointerCollector : MonoBehaviour
         }
     }
 
-    void zoneMovement() {
+    void zoneMovement()
+    {
         // Move the pointer towards the target position
         safeZone.position = Vector3.MoveTowards(safeZone.position, zoneTargetPosition, zoneSpeed * Time.deltaTime);
 
@@ -93,14 +95,14 @@ public class PointerCollector : MonoBehaviour
         else
         {
             Debug.Log("Fail!");
-            moveSpeed += 5f;
+            StartCoroutine(Wait3sec());
         }
     }
 
     IEnumerator Wait2sec()
     {
         canFishAgain = false;
-        GlobalPlayerVars.howHot-= 7;
+        GlobalPlayerVars.howHot -= 7;
         Debug.Log(GlobalPlayerVars.howHot);
 
         yield return new WaitForSeconds(2f);
@@ -109,5 +111,16 @@ public class PointerCollector : MonoBehaviour
         icecube.GetComponent<SpriteRenderer>().sprite = fishin;
         moveSpeed = oriSpeed;
         canFishAgain = true;
+    }
+
+    IEnumerator Wait3sec()
+    {
+        canFishAgain = false;
+
+        yield return new WaitForSeconds(3f);
+
+        moveSpeed += 5f;
+        canFishAgain = true;
+
     }
 }
